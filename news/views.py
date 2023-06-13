e239f8534fa1754a16818c95ceba890af1441e6b
Анастасия <121298392+anstsa@users.noreply.github.com>
2023-06-13 17:12:31 +0300

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class NewsList (ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-id')
+
+class NewDetail (DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
+
+
+
